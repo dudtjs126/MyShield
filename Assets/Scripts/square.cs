ed4157f010de35e60d30fd9bc353101fa48f7bfd
dudtjs126 <dudtjs126@gmail.com>
2023-12-29 11:07:02 +0900

    네모 박스 사라지기 기능 추가
    화면 밖에 벗어났을 때 네모 박스가 사라지도록 기능 추가

--- Assets/Scripts/square.cs
+++ Assets/Scripts/square.cs
@@ -18,7 +18,10 @@ public class square : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < -5.5f)
+        {
+            Destroy(gameObject);
+        }
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
